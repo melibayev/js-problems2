62243e65628797af6d809e97719c958d281770d2
“melibayev” <melibayefff1@gmail.com>
2023-05-25 05:45:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,128 @@
+// Boolean 1
+// let a = 5;
+
+// console.log(a%2 == 0);
+
+// Boolean 2
+// let a = 5;
+// let b = 8;
+// console.log(a >= 0 || b <-2);
+
+// Boolean 3
+// let a = 5;
+// let b = 8;
+// let c = 4;
+// console.log(a < b < c);
+
+// Boolean 4
+// let a = 5;
+// let b = 8;
+// console.log((a % 2 == 1) && (b % 2 == 1));
+
+// Boolean 5
+// let a = 5;
+// let b = 8;
+// console.log((a % 2 == 1) || (b % 2 == 1));
+
+// // Boolean 6
+// let a = 5;
+// let b = 8;
+// console.log(((a % 2 == 1) && (b % 2 == 0)) || ((a % 2 == 0) && (b % 2 == 1)));
+
+// // Boolean 7
+// let a = 5;
+// let b = 8;
+// let c = 10
+// console.log((a > 0)&&(b > 0)&&(c > 0));
+
+// // Boolean 8
+// let a = 5;
+// let b = 8;
+// let c = 10
+// console.log(((a < 0)&&(b < 0)&&(c > 0)) || ((a < 0)&&(b > 0)&&(c < 0)) || ((a > 0)&&(b < 0)&&(c < 0)));
+
+// // Boolean 9
+// let a = 5;
+// let b = 8;
+// let c = 10
+// console.log(((a < 0)&&(b > 0)&&(c > 0)) || ((a > 0)&&(b > 0)&&(c < 0)) || ((a > 0)&&(b < 0)&&(c > 0)));
+
+// // Boolean 10
+// let a = 5;
+// console.log((a / 10 >= 1) && (a%2==0));
+
+// // Boolean 11
+// let a = 5;
+// console.log((a / 100 >= 1) && (a % 2 == 1));
+
+// // Boolean 12
+// let a = 5;
+// let b = 8;
+// let c = 10
+// console.log((a - b == 0) || (a - c == 0) || (b - c == 0));
+
+// // Boolean 13
+// let a = 5;
+// let b = 8;
+// console.log();
+
+// // Boolean 14
+// let abc = 5;
+// let c = abc % 10;
+// let b = (abc % 100) - c;
+// let a = Math.trunc(abc / 100);
+// console.log(a < b < c);
+
+// // Boolean 15
+// let abc = 5;
+// let c = abc % 10;
+// let b = (abc % 100) - c;
+// let a = Math.trunc(abc / 100);
+// console.log((a < b < c) || (a > b && a > c));
+
+
+
+// let A = 3; 
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// // 1
+// let y1 = (-B/(2*(A**2+B**2)));
+// console.log(y1);
+
+// // 2
+// let y2 = ((A+(B/C)+D)**2)+B**2;
+// console.log(y2);
+
+// // 3
+// let y3 = 2 * ((A**D) + ((4*C**2)/3));
+// console.log(y3);
+
+// // 4
+// let y4 = (1/(A**2)) * ((B/10)**3) * ((C+D)**2);
+// console.log(y4);
+
+// // 5
+// let y5 = ((A/B - 1) * ((C/(D-1))**2));
+// console.log(y5);
+
+// // 6
+// let y6 = A + (B/C) + (D/A) + C;
+// console.log(y6);
+
+// // 7
+// let y7 = Math.sqrt(((A+B+C)**2) - ((A-B-C)**2));
+// console.log(y7);
+
+// // 8
+// let y8 = (A**2 + B**2 + C**2)/(A*B*C);
+// console.log(y8);
+
+// // 9
+// let y9 = ((A*D**3)/3) + ((B*C**2)/2);
+// console.log(y9);
+
+// // 10
+// let y10 = Math.abs((A/B)-(B/C)) + (4*Math.sqrt(A+C));
+// console.log(y10); 
